6b87d4905f90800465fc5a9804060f4b5e2d57fc
Abinu <asivabalan@spartaglobal.com>
2018-12-04 16:07:12 +0000

    15 tests added

--- faker.rb
+++ faker.rb
@@ -1,7 +1,11 @@
 require 'faker'
+require 'json'
+require 'httparty'
 
 class Generator
+  include HTTParty
 
+  base_uri 'api.postcodes.io'
 
   def name
     @first_name = Faker::Name.name
@@ -12,7 +16,7 @@ class Generator
   end
 
   def postcode_user
-    @postcode = Faker::Address.postcode
+    JSON.parse(self.class.get("/random/postcodes/").body)['result']['postcode']
   end
 
   def gender_user
@@ -35,6 +39,35 @@ class Generator
     Date.today.year - date_of_birth_user.year
   end
 
+  def phone_number_user
+    Faker::Config.locale = 'en-GB'
+    @phone_number = Faker::PhoneNumber.phone_number
+  end
+
+  def university_user
+    @university = Faker::University.name
+  end
+
+  def programming_language_user
+    @programming_language = Faker::ProgrammingLanguage.name
+  end
+
+  def bank_account_number_user
+    @bank_account_number = Faker::Bank.account_number
+  end
+
+  def company_industry_user
+    @company_industry = Faker::Company.industry
+  end
+
+  def company_user
+    @company = Faker::Company.name
+  end
+
+  def marital_status_user
+    @marital_status = Faker::Demographic.marital_status
+  end
+
 end
 
 test = Generator.new
@@ -46,3 +79,10 @@ puts test.postcode_user
 puts test.job_title
 puts test.date_of_birth_user
 puts test.age
+puts test.phone_number_user
+puts test.university_user
+puts test.programming_language_user
+puts test.bank_account_number_user
+puts test.company_industry_user
+puts test.company_user
+puts test.marital_status_user
